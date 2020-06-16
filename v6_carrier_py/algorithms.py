ed76ce0ab0566b9cd61a5952a891385e5641f404
Djura Smits <djura.smits@gmail.com>
2020-06-16 16:57:26 +0200

    Created correlation function

--- v6_carrier_py/algorithms.py
+++ v6_carrier_py/algorithms.py
@@ -1,5 +1,5 @@
-from vantage6.tools.util import info
 import pandas as pd
+from vantage6.tools.util import info
 
 
 def RPC_column_names(data: pd.DataFrame, *args, **kwargs):
@@ -12,3 +12,7 @@ def RPC_column_names(data: pd.DataFrame, *args, **kwargs):
     # what you return here is sent to the central server. So make sure
     # no privacy sensitive data is shared
     return data.columns.to_list()
+
+
+def RPC_correlation_matrix(data: pd.DataFrame, *args, **kwargs):
+    return data.corr()
