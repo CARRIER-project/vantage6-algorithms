fd1ed56de87495e6a09247030edef76aa286f28c
Djura Smits <djura.smits@gmail.com>
2020-06-22 12:07:51 +0200

    Added test to see if there will be a mixup between keys

--- tests/test_master.py
+++ tests/test_master.py
@@ -10,6 +10,8 @@ MOCK_TASK = {'id': ID}
 KEYS = ['GBAGeboorteJaar', 'GBAGeboorteMaand', 'GBAGeboorteDag', 'GBAGeslacht', 'GBAPostcode', 'GBAHuisnummer',
         'GBAToev']
 KEY_VALUES = [1987, 10, 30, 1, '1098ln', 11, 'b']
+COLUMN1 = 'column1'
+COLUMN2 = 'column2'
 
 
 def test_column_names_returns_column_set():
@@ -44,14 +46,14 @@ def test_raise_exception_when_task_timeout():
 def test_master_corr_matrix_is_combined_corr_matrix():
     client = create_base_mock_client()
 
-    df1 = pd.DataFrame({'id': [1], 'column1': [123]})
-    df2 = pd.DataFrame({'id': [1], 'column2': [321]})
+    df1 = pd.DataFrame({'id': [1], COLUMN1: [123]})
+    df2 = pd.DataFrame({'id': [1], COLUMN2: [321]})
 
     node_results = [df1, df2]
     client.get_results.return_value = node_results
 
     result = master.correlation_matrix(client, None, tries=TRIES, keys='id')
-    target = pd.DataFrame({'id': [1], 'column1': [123], 'column2': [321]}).corr()
+    target = pd.DataFrame({'id': [1], COLUMN1: [123], COLUMN2: [321]}).corr()
 
     pd.testing.assert_frame_equal(target, result)
 
@@ -59,34 +61,55 @@ def test_master_corr_matrix_is_combined_corr_matrix():
 def test_master_corr_matrix_joins_on_multiple_keys():
     client = create_base_mock_client()
 
-    df1 = pd.DataFrame(data=[KEY_VALUES + [123]], columns=KEYS + ['column1'])
-    df2 = pd.DataFrame(data=[KEY_VALUES + [321]], columns=KEYS + ['column2'])
+    df1 = pd.DataFrame(data=[KEY_VALUES + [123]], columns=KEYS + [COLUMN1])
+    df2 = pd.DataFrame(data=[KEY_VALUES + [321]], columns=KEYS + [COLUMN2])
 
     client.get_results.return_value = [df1, df2]
 
-    target = pd.DataFrame(data=[KEY_VALUES + [123, 321]], columns=KEYS + ['column1', 'column2']).corr()
+    target = pd.DataFrame(data=[KEY_VALUES + [123, 321]], columns=KEYS + [COLUMN1, COLUMN2]).corr()
     result = master.correlation_matrix(client, None, keys=KEYS, tries=TRIES)
 
     pd.testing.assert_frame_equal(target, result)
 
 
-def if_no_keys_provided_infer_keys():
-    keys = ['GBAGeboorteJaar', 'GBAGeboorteMaand', 'GBAGeboorteDag', 'GBAGeslacht', 'GBAPostcode', 'GBAHuisnummer',
-            'GBAToev']
+def test_if_no_keys_provided_infer_keys():
     client = create_base_mock_client()
     key_values = [1987, 10, 30, 1, '1098ln', 11, 'b']
 
-    df1 = pd.DataFrame(data=[key_values + [123]], columns=keys + ['column1'])
-    df2 = pd.DataFrame(data=[key_values + [321]], columns=keys + ['column2'])
+    df1 = pd.DataFrame(data=[key_values + [123]], columns=KEYS + [COLUMN1])
+    df2 = pd.DataFrame(data=[key_values + [321]], columns=KEYS + [COLUMN2])
 
     client.get_results.return_value = [df1, df2]
 
-    target = pd.DataFrame(data=[key_values + [123, 321]], columns=keys + ['column1', 'column2']).corr()
+    target = pd.DataFrame(data=[key_values + [123, 321]], columns=KEYS + [COLUMN1, COLUMN2]).corr()
 
     # This time no keys are specified, but it should still give the expected result
     result = master.correlation_matrix(client, None, tries=TRIES)
 
     pd.testing.assert_frame_equal(target, result)
+
+
+def test_dont_mix_up_partly_matching_keys():
+    client = create_base_mock_client()
+
+    # Two different people with the same birth date
+    person1 = [1987, 10, 30, 1, '1098ln', 11, 'b']
+    person2 = [1987, 10, 30, 1, '1098ln', 12, '']
+
+    data_left = [person1 + [1], person2 + [0]]
+    data_right = [person1 + [1], person2 + [0]]
+
+    pd_left = pd.DataFrame(data_left, columns=KEYS + [COLUMN1])
+    pd_right = pd.DataFrame(data_right, columns=KEYS + [COLUMN2])
+
+    client.get_results.return_value = [pd_left, pd_right]
+
+    # If data is merged correctly, correlation between column1 and column2 should be 1, otherwise it is 0.5
+    result = master.correlation_matrix(client, None, tries=TRIES)
+    assert result[COLUMN1][COLUMN2] == 1
+
+
+
 
 
 def create_base_mock_client():
